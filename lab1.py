a4ee68063f817f6cf6ea7f18300f2bcc0f34fcea
Zurihaa <tle33@hawk.iit.edu>
2024-09-12 18:06:35 -0500

    Update Lab1 to satisfy HW requirements

--- lab1.py
+++ lab1.py
@@ -1,59 +1,62 @@
 def encrypt(etext,n):
-    ans = ""
+    encrypted = ""
     for i in range(len(etext)):
         ch = etext[i]
         if ch==" ":
-            ans+=" "
+            encrypted+=" "
         elif (ch.isupper()):
-            ans += chr((ord(ch) + n-65) % 26 + 65)        
+            encrypted += chr((ord(ch) + n-65) % 26 + 65)        
         else:
-            ans += chr((ord(ch) + n-97) % 26 + 97)
-    return ans
+            encrypted += chr((ord(ch) + n-97) % 26 + 97)
+    return encrypted
 
 def decrypt(dtext,n):        
-    letters="abcdefghijklmnopqrstuvwxyz"    
-    decrypted_message = ""
-
-    for ch in dtext:
-
-        if ch in letters:
-            position = letters.find(ch)
-            new_pos = (position - n) % 26
-            new_char = letters[new_pos]
-            decrypted_message += new_char
+    decrypted = ""
+    for i in range(len(dtext)):
+        ch = dtext[i]
+        if ch==" ":
+            decrypted+=" "
+        elif (ch.isupper()):
+            decrypted += chr((ord(ch) - n - 65) % 26 + 65)        
         else:
-            decrypted_message += ch
+            decrypted += chr((ord(ch) - n - 97) % 26 + 97)
+    return decrypted
 
 def brutefore(btext):
-    letters="abcdefghijklmnopqrstuvwxyz"    
 
-    for key in range(len(letters)):
-        translated = ''
-        for ch in btext:
-            if ch in letters:
-                num = letters.find(ch)
-                num = num - key
-                if num < 0:
-                    num = num + len(letters)
-                translated = translated + letters[num]
+    for key in range(1, len(btext) + 1):
+        brutetext = ''
+        for i in range(len(btext)):
+            ch = btext[i]
+            if ch == " ":
+                brutetext += " "
+            elif (ch.isupper()):
+                brutetext += chr((ord(ch) + key - 65) % 26 + 65)        
             else:
-                translated = translated + ch
+                brutetext += chr((ord(ch) + key - 97) % 26 + 97)
+        print('Key #%s: %s' % (key, brutetext))
 
 print("Choose an option:")
 print("1. Encryption")
 print("2. Decryption")
 print("3. Brute Force")
-choice = int(input("Enter your choice (1/2/3): "))
-
-plaintext = input("Enter plaintext: ")
-n = int(input("Enter key: "))
-
+while True:
+    try:
+        choice = int(input("Enter your choice (1/2/3): "))
+        if choice not in [1, 2, 3]:
+            raise ValueError("Choice must be 1, 2, or 3.")
+        break
+    except ValueError:
+        print("Invalid input. Please enter 1, 2, or 3.")
+        
 if choice == 1:
+    plaintext = input("Enter plaintext: ")
+    n = int(input("Enter key: "))
     print("Ciphertext: ",encrypt(plaintext,n))
 elif choice == 2:
+    plaintext = input("Enter plaintext: ")
+    n = int(input("Enter key: "))
     print("Ciphertext: ",decrypt(plaintext,n))
-elif choice == 3:
+elif choice == 3: 
+    plaintext = input("Enter plaintext: ")
     print("Ciphertext: ",brutefore(plaintext))
-
-
-    
